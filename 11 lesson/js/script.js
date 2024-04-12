42013835a64ce5ae6023045305b06b284adde482
username <andrey.bondar12052002@gmail.com>
2024-04-12 16:23:11 +0300

    2 commit

--- 11 lesson/js/script.js
+++ 11 lesson/js/script.js
@@ -1,36 +1,43 @@
 // task1
 {
-let kilometres = +prompt("Яку кількість кілометрів ти готовий бігати щодня?");
-let wishtKilometres = +prompt("Яку кількість усіх кілометрів ти хочеш пробігти за місяць?");
-if(wishtKilometres <= kilometres){
-    alert('Ти зможеш пробігти усю заплановану дистанцію');
-}else{
-    var difference = wishtKilometres - kilometres;
-    alert("Краще зменши сумарну кількість кілометрів на " + difference + "км" );
-}
+  const kilometres = +prompt(
+    "Яку кількість кілометрів ти готовий бігати щодня?"
+  );
+  const wishtKilometres = +prompt(
+    "Яку кількість усіх кілометрів ти хочеш пробігти за місяць?"
+  );
+  if (wishtKilometres <= kilometres) {
+    alert("Ти зможеш пробігти усю заплановану дистанцію");
+  } else {
+    const difference = wishtKilometres - kilometres;
+    alert("Краще зменши сумарну кількість кілометрів на " + difference + "км");
+  }
 }
 // task2
 {
-    const credentials = {
-        login: 'admin',
-        password: 'qwerty'
-    };
-    
-    const form = document.getElementById('loginForm');
-    const resultDiv = document.getElementById('result');
-    
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-    
-        const enteredLogin = document.getElementById('login').value;
-        const enteredPassword = document.getElementById('password').value;
-    
-        if (enteredLogin === credentials.login && enteredPassword === credentials.password) {
-            resultDiv.textContent = 'Welcome, ' + enteredLogin + '!';
-            resultDiv.className = 'success';
-        } else {
-            resultDiv.textContent = 'Invalid login or password.';
-            resultDiv.className = 'error';
-        }
-    });
+  const credentials = {
+    login: "admin",
+    password: "qwerty",
+  };
+
+  const form = document.getElementById("loginForm");
+  const resultDiv = document.getElementById("result");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const enteredLogin = document.getElementById("login").value;
+    const enteredPassword = document.getElementById("password").value;
+
+    if (
+      enteredLogin === credentials.login &&
+      enteredPassword === credentials.password
+    ) {
+      resultDiv.textContent = "Welcome, " + enteredLogin + "!";
+      resultDiv.className = "success";
+    } else {
+      resultDiv.textContent = "Invalid login or password.";
+      resultDiv.className = "error";
+    }
+  });
 }
